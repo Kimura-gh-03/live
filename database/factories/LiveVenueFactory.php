cbf5e7758fbcaf7730040116a99f35421c214434
Kimura-gh-03 <kmrmrk0316@gmail.com>
2026-02-27 17:24:47 +0900

    feat: ライブ会場モデルに緯度と経度のフィールドを追加し、シーダーとマイグレーションを更新

--- database/factories/LiveVenueFactory.php
+++ database/factories/LiveVenueFactory.php
@@ -18,6 +18,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '福住駅',
             'access' => '徒歩約10分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=大和ハウス+プレミストドーム&query_place_id=ChIJx289pMEqC18Ri7aK56qxdio',
+            'latitude' => 43.0151,
+            'longitude' => 141.4145,
             'toilet_layout' => [
                 'female_locations' => '約25箇所',
                 'male_locations' => '約20箇所',
@@ -31,6 +33,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '利府駅',
             'access' => 'バス約10分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=セキスイハイムスーパーアリーナ&query_place_id=ChIJjchmBHGEiV8Rcukw2geTA7c',
+            'latitude' => 38.3390,
+            'longitude' => 141.0127,
             'toilet_layout' => [
                 'female_locations' => '約6箇所',
                 'male_locations' => '約5箇所',
@@ -44,6 +48,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => 'さいたま新都心駅',
             'access' => '徒歩約3分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=さいたまスーパーアリーナ&query_place_id=ChIJv4Yp2QzBGGARnYRUdDcabFA',
+            'latitude' => 35.8938,
+            'longitude' => 139.6287,
             'toilet_layout' => [
                 'female_locations' => '計18箇所',
                 'male_locations' => '計14箇所',
@@ -57,6 +63,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '九段下駅',
             'access' => '徒歩約5分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=日本武道館&query_place_id=ChIJE_QrgGuMGGARiHFlyXFcWyE',
+            'latitude' => 35.6935,
+            'longitude' => 139.7496,
             'toilet_layout' => [
                 'female_locations' => '約6箇所',
                 'male_locations' => '約5箇所',
@@ -70,6 +78,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '横浜駅 / 新高島駅',
             'access' => '徒歩約9〜11分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=Kアリーナ横浜&query_place_id=ChIJ6Te0zGZdGGARg8sCj6TN9rQ',
+            'latitude' => 35.4657,
+            'longitude' => 139.6280,
             'toilet_layout' => [
                 'female_locations' => '計15箇所',
                 'male_locations' => '計10箇所',
@@ -83,6 +93,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '名城公園駅',
             'access' => '徒歩約2分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=IGアリーナ&query_place_id=ChIJ9WgWji1xA2ARf6xDT5v8b4s',
+            'latitude' => 35.1839,
+            'longitude' => 136.8997,
             'toilet_layout' => [
                 'female_locations' => '約12箇所',
                 'male_locations' => '約8箇所',
@@ -96,6 +108,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => 'ドーム前千代崎駅',
             'access' => '徒歩すぐ',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=京セラドーム大阪&query_place_id=ChIJS2iHS7LnAGAR8YQUFvl1X1M',
+            'latitude' => 34.6693,
+            'longitude' => 135.4758,
             'toilet_layout' => [
                 'female_locations' => '20箇所以上',
                 'male_locations' => '15箇所以上',
@@ -109,6 +123,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '桜島駅 (バス利用)',
             'access' => 'バス約15分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=おおきにアリーナ舞洲&query_place_id=ChIJ1YxPRuzpAGAR1Puy_XjxwZ8',
+            'latitude' => 34.6655,
+            'longitude' => 135.3996,
             'toilet_layout' => [
                 'female_locations' => '1F・2F計4箇所',
                 'male_locations' => '1F・2F計4箇所',
@@ -122,6 +138,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '呉服町駅 / 中洲川端駅',
             'access' => '徒歩約15分 / バス約10分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=マリンメッセ福岡+A館&query_place_id=ChIJmWyYFuWRQTURMXvsTy8vXro',
+            'latitude' => 33.6023,
+            'longitude' => 130.4232,
             'toilet_layout' => [
                 'female_locations' => '約10箇所',
                 'male_locations' => '約7箇所',
@@ -135,6 +153,8 @@ class LiveVenueFactory extends Factory
             'nearest_station' => '那覇空港 (高速バス)',
             'access' => '沖縄市運動公園前 徒歩約5分',
             'google_maps_url' => 'https://www.google.com/maps/search/?api=1&query=沖縄アリーナ&query_place_id=ChIJdwMAGWkT5TQR1FyodU4qN9Q',
+            'latitude' => 26.3347,
+            'longitude' => 127.8059,
             'toilet_layout' => [
                 'female_locations' => '約10箇所',
                 'male_locations' => '約8箇所',
